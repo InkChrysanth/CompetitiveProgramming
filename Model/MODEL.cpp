588839eb62aa4450fb51e2ef9842dee0bbabede1
kx23333 <2978299679@qq.com>
2023-08-27 21:10:01 +0800

    Create the common model

---
+++ Model/MODEL.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	return 0;
+}
