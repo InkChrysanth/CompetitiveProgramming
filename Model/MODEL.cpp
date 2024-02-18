7210dc74794c501ffa4539aaf8cf31c84fec1156
kx23333 <2978299679@qq.com>
2024-02-18 16:14:01 +0800

    modified

--- Model/MODEL.cpp
+++ Model/MODEL.cpp
@@ -1,22 +1,48 @@
-// #pragma GCC optimize(2)
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
 #include <bits/stdc++.h>
-#define int_rd (int)read()
-#define ll_rd read()
+#define Fi(s) freopen(s,"r",stdin)
+#define Fo(s) freopen(s,"w",stdout)
+#define Fre(s) Fi(s".in"),Fo(s".out")
+#define For(i,j,k) for(int i=(j),i##_=(k);i<=i##_;i++)
+#define Rep(i,j,k) for(int i=(j),i##_=(k);i>=i##_;i--)
+#define fi first
+#define se second
+#define rd read
 #define endl '\n'
 #define mp make_pair
+#define eb emplace_back
 
 using namespace std;
 
 typedef long long ll;
+typedef unsigned long long ull;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-inline ll read()
+const int inf=0x3f3f3f3f;
+const ll llinf=0x3f3f3f3f3f3f3f3fll;
+
+template <typename T>
+void read(T &x)
 {
-	ll a=0; int f=0; char p=getchar();
-	while(!isdigit(p)){f|=p=='-'; p=getchar();}
-	while(isdigit(p)){a=(a<<3)+(a<<1)+(p^48); p=getchar();}
-	return f?-a:a;
+	x=0; char ch=getchar(); ll f=1;
+	while(!isdigit(ch))
+	{
+		if(ch=='-') f*=-1;
+		ch=getchar();
+	}
+	while(isdigit(ch))
+	{
+		x=x*10+(ch^48);
+		ch=getchar();
+	}
+	x*=f;
+}
+template <typename T, typename...Args>
+void read(T &first, Args &...args)
+{
+	read(first);
+	read(args...);
 }
 
 signed main()
