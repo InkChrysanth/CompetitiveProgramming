146538b0ba4cd691ffa2ae9d897321c0cf25f545
kx23333 <2978299679@qq.com>
2024-07-04 17:08:56 +0800

    added

---
+++ AtCoder/AT_abc303_e.cpp
@@ -0,0 +1,83 @@
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
+#include <bits/stdc++.h>
+#define Fi(s) freopen(s,"r",stdin)
+#define Fo(s) freopen(s,"w",stdout)
+#define Fre(s) Fi(s".in"),Fo(s".out")
+#define all(s) s.begin(), s.end()
+#define For(i,j,k) for(int i=(j),i##_=(k);i<=i##_;i++)
+#define Rep(i,j,k) for(int i=(j),i##_=(k);i>=i##_;i--)
+#define fi first
+#define se second
+#define rd read
+#define endl '\n'
+#define mp make_pair
+#define eb emplace_back
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+
+const int inf=0x3f3f3f3f;
+const ll llinf=0x3f3f3f3f3f3f3f3fll;
+
+int dx[]={0, 0, 1, 0, -1, 1, 1, -1, -1};
+int dy[]={0, 1, 0, -1, 0, 1, -1, 1, -1};
+
+template <typename T>
+void read(T &x)
+{
+	x=0; char ch=getchar(); ll f=1;
+	while(!isdigit(ch))
+	{
+		if(ch=='-') f*=-1;
+		ch=getchar();
+	}
+	while(isdigit(ch))
+	{
+		x=x*10+(ch^48);
+		ch=getchar();
+	}
+	x*=f;
+}
+template <typename T, typename...Args>
+void read(T &first, Args &...args)
+{
+	read(first);
+	read(args...);
+}
+
+const int N=200010;
+
+int n;
+int d[N];
+
+vector<int> ans;
+
+signed main()
+{
+	cin.tie(0); cout.tie(0);
+    rd(n);
+    For(i, 1, n-1)
+    {
+        int u, v;
+        rd(u, v);
+        d[u]++, d[v]++;
+    }
+    int cnt=n;
+    For(i, 1, N-10)
+    {
+        if(d[i]>2)
+        {
+            ans.push_back(d[i]);
+            cnt-=(d[i]+1);
+        }
+    }
+    cnt/=3;
+    For(i, 1, cnt) cout<<2<<' ';
+    sort(all(ans));
+    for(auto x: ans) cout<<x<<' ';
+	return 0;
+}
