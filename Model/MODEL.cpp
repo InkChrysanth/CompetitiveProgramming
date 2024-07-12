0cdc201d8571f2238b7fbeb95965e38696040f1e
kx23333 <2978299679@qq.com>
2024-07-12 21:05:40 +0800

    modified

--- Model/MODEL.cpp
+++ Model/MODEL.cpp
@@ -1,5 +1,7 @@
 //#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
 #include <bits/stdc++.h>
+#define cmin(i,j) (i)=min((i),(j))
+#define cmax(i,j) (i)=max((i),(j))
 #define debug(x) cerr<<#x<<": "<<(x)<<endl
 #define Fi(s) freopen(s,"r",stdin)
 #define Fo(s) freopen(s,"w",stdout)
@@ -12,8 +14,8 @@
 #define se second
 #define rd read
 #define endl '\n'
+#define pq priority_queue
 #define mp make_pair
-#define mtp make_tuple
 #define pb push_back
 #define eb emplace_back
 #define Yes cout<<"Yes"<<endl
