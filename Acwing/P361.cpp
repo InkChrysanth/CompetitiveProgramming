320de5cd6a109c750d08347fbeef7d9a1f891dd3
kx233 <2978299679@qq.com>
2024-07-30 14:28:02 +0800

    Updated

--- Acwing/P361.cpp
+++ Acwing/P361.cpp
@@ -1,72 +1,93 @@
+// Problem: 观光奶牛
+// Contest: AcWing
+// URL: https://www.acwing.com/problem/content/description/363/
+// Memory Limit: 64 MB
+// Time Limit: 1000 ms
+// 
+// Powered by CP Editor (https://cpeditor.org)
+
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
 #include <bits/stdc++.h>
 using namespace std;
-const int N=1010, M=5010;
-int n, m;
-int wf[N];
-int h[N], e[M], ne[M], wt[M], idx;
-int cnt[N], q[N];
-double dist[N];
-bool st[N];
 
-void add(int a, int b, int c)
-{
-    e[idx]=b, wt[idx]=c, ne[idx]=h[a], h[a]=idx++;
-}
+#define cmin(i,j) (i)=min((i),(j))
+#define cmax(i,j) (i)=max((i),(j))
+#define debug(x) cerr<<#x<<": "<<(x)<<endl
+#define Fi(s) freopen(s,"r",stdin)
+#define Fo(s) freopen(s,"w",stdout)
+#define Fre(s) Fi(s".in"),Fo(s".out")
+#define all(x) x.begin(), x.end()
+#define each(i,x) for(auto &i:(x))
+#define fi first
+#define se second
+#define endl '\n'
+#define pq priority_queue
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
 
-bool check(double mid)
-{
-    memset(dist, 0, sizeof dist);
-    memset(st, false, sizeof st);
-    memset(cnt, 0, sizeof cnt);
-    int hh=0, tt=0;
-    for(int i=1; i<=n; i++)
-    {
-        q[tt++]=i;
-        st[i]=true;
-    }
-    while(hh!=tt)
-    {
-        int t=q[hh++];
-        if(hh==N) hh=0;
-        st[t]=false;
-        for(int i=h[t]; ~i; i=ne[i])
-        {
-            int j=e[i];
-            if(dist[j]<dist[t]+wf[t]-mid*wt[i])
-            {
-                dist[j]=dist[t]+wf[t]-mid*wt[i];
-                cnt[j]=cnt[t]+1;
-                if(cnt[j]>=n) return true;
-                if(!st[j])
-                {
-                    st[j]=true;
-                    q[tt++]=j;
-                    if(tt==N) tt=0;
-                }
-            }
-        }
-    }
-    return false;
-}
+using ll=long long;
+using ld=long double;
+using pii=pair<int, int>;
+using pli=pair<ll, int>;
 
-int main()
+constexpr int inf=0x3f3f3f3f;
+constexpr ll llinf=0x3f3f3f3f3f3f3f3fll;
+constexpr int N=1010;
+constexpr double eps=1e-4;
+
+vector<pii> g[N];
+
+signed main()
 {
-    memset(h, -1, sizeof h);
-    cin.tie(0); cin>>n>>m;
-    for(int i=1; i<=n; i++)
-        cin>>wf[i];
-    for(int i=1; i<=m; i++)
-    {
-        int a, b, c; cin>>a>>b>>c;
-        add(a, b, c);
-    }
-    double l=0, r=100010, mid;
-    while(r-l>1e-4)
-    {
-        mid=(l+r)/2;
-        if(check(mid)) l=mid;
-        else r=mid;
-    }
-    printf("%.2lf", mid);
-    return 0;
+	cout<<fixed<<setprecision(2);
+	cin.tie(0)->sync_with_stdio(0);
+	int n, m;
+	cin>>n>>m;
+	vector<int> a(n+1);
+	for(int i=1; i<=n; i++) cin>>a[i];
+	for(int i=1, u, v, w; i<=m; i++)
+	{
+		cin>>u>>v>>w;
+		g[u].eb(mp(v, w));
+	}
+	auto check=[&](double mid) -> bool
+	{
+		vector<double> dist(n+1, 0);
+		vector<int> cnt(n+1, 0);
+		vector<bool> ins(n+1, 0);
+		queue<int> q;
+		for(int i=1; i<=n; i++)
+			q.push(i), ins[i]=1;
+		while(q.size())
+		{
+			int u=q.front();
+			q.pop();
+			ins[u]=false;
+			each(t, g[u])
+			{
+				int v=t.fi;
+				//sigma fi/sigma ti >= mid
+				//sigma (ti * mid - fi) <= 0
+				double w=t.se*mid-a[u];
+				if(dist[v]>dist[u]+w)
+				{
+					dist[v]=dist[u]+w;
+					cnt[v]=cnt[u]+1;
+					if(cnt[v]>=n) return 1;
+					if(!ins[v]) q.push(v), ins[v]=1;
+				}
+			}
+		}
+		return 0;
+	};
+	double l=0, r=1000, ans=r;
+	while(l<=r)
+	{
+		double mid=(l+r)/2;
+		if(check(mid)) l=mid+eps, ans=mid;
+		else r=mid-eps;
+	}
+	cout<<ans<<endl;
+	return 0;
 }
