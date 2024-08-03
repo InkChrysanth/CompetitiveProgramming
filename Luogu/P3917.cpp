51bbbdb1fde6745b87f88b98ab020bb72d51b251
kx23333 <2978299679@qq.com>
2024-08-03 21:40:37 +0800

    added

---
+++ Luogu/P3917.cpp
@@ -0,0 +1,46 @@
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,inline")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define cmin(i,j) (i)=min((i),(j))
+#define cmax(i,j) (i)=max((i),(j))
+#define debug(x) cerr<<#x<<": "<<(x)<<endl
+#define Fi(s) freopen(s,"r",stdin)
+#define Fo(s) freopen(s,"w",stdout)
+#define Fre(s) Fi(s".in"),Fo(s".out")
+#define all(x) x.begin(), x.end()
+#define each(i,x) for(auto &i:(x))
+#define fi first
+#define se second
+#define endl '\n'
+#define pq priority_queue
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+
+using ll=long long;
+using ld=long double;
+using pii=pair<int, int>;
+using pli=pair<ll, int>;
+
+constexpr int inf=0x3f3f3f3f;
+constexpr ll llinf=0x3f3f3f3f3f3f3f3fll;
+
+signed main()
+{
+	cin.tie(0)->sync_with_stdio(0);
+    int n, x; ll ans=0;
+    cin>>n;
+    vector<ll> f(32, 0ll);
+    for(int i=1; i<=n; i++)
+    {
+        cin>>x;
+        for(int j=0; j<=30; j++)
+        {
+            if((x>>j)&1) f[j]=i-f[j];
+            ans+=f[j]*(1<<j);
+        }
+    }
+    cout<<ans<<endl;
+	return 0;
+}
