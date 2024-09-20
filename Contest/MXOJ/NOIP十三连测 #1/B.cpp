56323e4298be1ef3b0b00fca3662b122c05e07bf
kx23333 <2978299679@qq.com>
2024-09-20 08:29:29 +0800

    added

--- Contest/MXOJ/NOIP十三连测 #1/B.cpp
+++ Contest/MXOJ/NOIP十三连测 #1/B.cpp
@@ -1,61 +1,53 @@
-//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops")
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,inline")
 #include <bits/stdc++.h>
+using namespace std;
+
+#define cmin(i,j) (i)=min((i),(j))
+#define cmax(i,j) (i)=max((i),(j))
 #define debug(x) cerr<<#x<<": "<<(x)<<endl
 #define Fi(s) freopen(s,"r",stdin)
 #define Fo(s) freopen(s,"w",stdout)
 #define Fre(s) Fi(s".in"),Fo(s".out")
-#define all(s) s.begin(), s.end()
-#define For(i,j,k) for(int i=(j),i##_=(k);i<=i##_;i++)
-#define Rep(i,j,k) for(int i=(j),i##_=(k);i>=i##_;i--)
+#define all(x) x.begin(), x.end()
+#define each(i,x) for(auto &i:(x))
 #define fi first
 #define se second
-#define rd read
-#define endl '\n'
+#define endl "\n"
+#define pq priority_queue
 #define mp make_pair
+#define pb push_back
 #define eb emplace_back
-#define Yes cout<<"Yes"<<endl
-#define No cout<<"No"<<endl
+#define em emplace
 
-using namespace std;
+using ll=long long;
+using ull=unsigned long long;
+using ld=long double;
+using pii=pair<int, int>;
+using pll=pair<ll, ll>;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-
-const int inf=0x3f3f3f3f;
-const ll llinf=0x3f3f3f3f3f3f3f3fll;
-
-int dx[]={0, 0, 1, 0, -1, 1, 1, -1, -1};
-int dy[]={0, 1, 0, -1, 0, 1, -1, 1, -1};
-
-template <typename T>
-void read(T &x)
-{
-	x=0; char ch=getchar(); ll f=1;
-	while(!isdigit(ch))
-	{
-		if(ch=='-') f*=-1;
-		ch=getchar();
-	}
-	while(isdigit(ch))
-	{
-		x=x*10+(ch^48);
-		ch=getchar();
-	}
-	x*=f;
-}
-template <typename T, typename...Args>
-void read(T &first, Args &...args)
-{
-	read(first);
-	read(args...);
-}
+constexpr int inf=0x3f3f3f3f;
+constexpr ll llinf=0x3f3f3f3f3f3f3f3fll;
 
 signed main()
 {
-	cin.tie(0); cout.tie(0);
-	Fre("Btree");
-	cout<<-1<<endl;
+    Fre("Btree");
+    cin.tie(0)->sync_with_stdio(0); 
+    int n, k;
+    cin>>n>>k;
+    if(!(n&1)) return cout<<"-1"<<endl, 0;
+    int h=__gcd(n, k), w=n/h;
+    vector<vector<int>> g(h+1, vector<int>(w+1));
+    for(int i=1; i<=h; i++)
+    {
+        g[i][1]=i-1;
+        for(int j=2; j<=w; j++)
+            g[i][j]=(g[i][j-1]+k)%n;
+    }
+    cout<<(n-1)/2<<endl;
+    for(int i=1; i<h; i++)
+        for(int j=1; j<w; j+=2)
+            cout<<g[i][j]<<" "<<g[i+1][j]<<endl;
+    for(int i=1; i<=w-2; i+=2) cout<<g[1][i]<<" "<<g[1][i+1]<<endl;
+    for(int i=2; i<h; i+=2) cout<<g[i][w]<<" "<<g[i+1][w-1]<<endl;
 	return 0;
 }
