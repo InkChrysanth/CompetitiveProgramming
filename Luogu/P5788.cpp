34361b12bd0fb190ae4811c79a8e820a8419cb13
kx23333 <2978299679@qq.com>
2024-10-26 10:08:48 +0800

    added

---
+++ Luogu/P5788.cpp
@@ -0,0 +1,62 @@
+//#pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,inline")
+#include <bits/stdc++.h>
+using namespace std;
+
+#define cmin(i,j) (i)=min((i),(j))
+#define cmax(i,j) (i)=max((i),(j))
+#define debug(x) cerr<<#x<<": "<<(x)<<endl
+#define all(x) x.begin(), x.end()
+#define each(i,x) for(auto &i:(x))
+#define fi first
+#define se second
+#define endl "\n"
+#define pq priority_queue
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define ins insert
+
+using ll=long long;
+using ull=unsigned long long;
+using db=double;
+using ld=long double;
+using pii=pair<int, int>;
+using pll=pair<ll, ll>;
+
+inline namespace FileIO{
+void setIn(string s) { freopen(s.c_str(), "r", stdin); }
+void setOut(string s) { freopen(s.c_str(), "w", stdout); }
+void setIO(string s="")
+{	
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+	#ifndef LOCAL
+        if(s.size()) setIn(s+".in"), setOut(s+".out");
+	#else
+        setIn("inkorange.in"), setOut("inkorange.out");
+    #endif
+}
+}
+
+constexpr int inf=0x3f3f3f3f;
+constexpr ll llinf=0x3f3f3f3f3f3f3f3fll;
+constexpr int N=3000010;
+
+int n;
+int a[N], ans[N];
+int que[N], top=0;
+
+signed main()
+{
+	setIO();
+	cin>>n;
+    for(int i=1; i<=n; i++) cin>>a[i];
+    for(int i=n; i>=1; i--)
+    {
+        while(top && a[que[top]]<=a[i]) top--;
+        ans[i]=que[top];
+        que[++top]=i;
+    }
+    for(int i=1; i<=n; i++) cout<<ans[i]<<" ";
+	return 0;
+}
