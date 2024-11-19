bdb07abb7721b6dca841f77d97e40a7a04601176
kx23333 <2978299679@qq.com>
2024-11-19 15:34:00 +0800

    modified

--- Contest/Others/2024-11-18 校内模拟赛/E.cpp
+++ Contest/Others/2024-11-18 校内模拟赛/E.cpp
@@ -1,37 +1,76 @@
-#include<bits/stdc++.h>
-#define int long long
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,inline")
+#include <bits/stdc++.h>
 using namespace std;
-int n, a[100009], sum, ans, b[2];
-signed main() {
-	cin >> n;
-	for(int i = 1; i <= n; i++){
-		cin >> a[i];
-		sum += a[i];
+
+#define cmin(i,j) (i)=min((i),(j))
+#define cmax(i,j) (i)=max((i),(j))
+#define debug(x) cerr<<#x<<": "<<(x)<<endl
+#define all(x) x.begin(), x.end()
+#define each(i,x) for(auto &i:(x))
+#define fi first
+#define se second
+#define endl "\n"
+#define pq priority_queue
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define ins insert
+
+using ll=long long;
+using ull=unsigned long long;
+using db=double;
+using ld=long double;
+using pii=pair<int, int>;
+using pll=pair<ll, ll>;
+
+inline namespace FileIO{
+void setIn(string s) { freopen(s.c_str(), "r", stdin); }
+void setOut(string s) { freopen(s.c_str(), "w", stdout); }
+void setIO(string s="")
+{	
+	cin.tie(0)->sync_with_stdio(0);
+	cin.exceptions(cin.failbit);
+	#ifndef LOCAL
+        if(s.size()) setIn(s+".in"), setOut(s+".out");
+	#else
+        setIn("inkorange.in"), setOut("inkorange.out");
+    #endif
+}
+}
+
+constexpr int inf=0x3f3f3f3f;
+constexpr ll llinf=0x3f3f3f3f3f3f3f3fll;
+
+signed main()
+{
+	setIO();
+	int n; cin>>n; ll sum=0;
+	vector<ll> a(n+1);
+	for(int i=1; i<=n; i++) cin>>a[i], sum+=a[i];
+	ll ans=0; array<ll, 2> b={0, 0};// b_0: max  b_1: sub-max
+	for(int i=1; i<=n; i++)
+	{
+		if(a[i]>sum/3) return cout<<-1<<endl, 0;
+		ans+=a[i]/2;
+		if(a[i]/2>b[0]) b[1]=b[0]/*alert: notice the order*/, b[0]=a[i]/2;
+		else if(a[i]/2>b[1]) b[1]=a[i]/2;
 	}
-	for(int i = 1; i <= n; i++) {
-		if(a[i] * 3 > sum){
-			cout << -1;
-			return 0;
-		}
-		ans += a[i] / 2;
-		if(a[i] / 2 > b[0])// b[0] 存放最大, b[1] 存放次大
-			b[0] = a[i] / 2;
-		if(b[0] > b[1])
-			swap(b[0], b[1]);
-	}
-	while(true) { // 防止减完次大值之后，最大值 * 3 又大于 ans，所以进行循环 
-		bool flag = 1;
-		for(int i = 0; i <= 1; i++) {
-			if(b[i] * 3 > ans) {
-				flag = 0;
-				int x = (3 * b[i] - ans + 1) / 2; 
-				// 找到最小的 x 使得 b[i] * 3 <= ans，推不出此公式可以使用更复杂的分类讨论，并代值验算其正确性 
-				b[i] -= x;
-				ans -= x;
+	debug(b[0]); debug(b[1]);
+	while(true)
+	{
+		bool flag=true;
+		for(int i=0; i<=1; i++)
+		{
+			if(b[i]*3>ans)
+			{
+				flag=false;
+				ll x=(3*b[i]-ans+1)/2;
+				b[i]-=x, ans-=x;
 			}
 		}
-		if(flag)	break;
+		if(flag) break;
 	}
-	cout << min(ans / 3, sum / 9);
+	cout<<min(ans/3, sum/9)<<endl;
 	return 0;
 }
+// math, implementation
